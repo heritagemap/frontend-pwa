bb65d2e25e77483fa590583f540d213a9bba427e
Alexey Avdeev <aad.jerry@gmail.com>
2020-04-14 00:56:20 +0300

    chore: add docker

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use('/_api', createProxyMiddleware({
+  target: 'https://tools.wmflabs.org/ru_monuments/monmap/api.php',
+  changeOrigin: true,
+}));
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.use('/_api', createProxyMiddleware({ target: 'https://tools.wmflabs.org/ru_monuments/monmap/api.php', changeOrigin: true }));
 
 app.listen(9000);
