dd4fc8b82b29a1d2ca031158a1669f9496f92035
Alexey Avdeev <aad.jerry@gmail.com>
2020-04-14 22:46:25 +0300

    feat: change api

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Map from 'components/Map';
 import Sidebar from 'components/Sidebar';
 import './App.scss';
 
-const PAGES_RESOURCE = 'https://tools.wmflabs.org/ru_monuments/monmap/api.php?query=list-pages&prefix=%D0%9A%D1%83%D0%BB%D1%8C%D1%82%D1%83%D1%80%D0%BD%D0%BE%D0%B5_%D0%BD%D0%B0%D1%81%D0%BB%D0%B5%D0%B4%D0%B8%D0%B5_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8';
+const PAGES_RESOURCE = '/_api?query=list-pages&prefix=%D0%9A%D1%83%D0%BB%D1%8C%D1%82%D1%83%D1%80%D0%BD%D0%BE%D0%B5_%D0%BD%D0%B0%D1%81%D0%BB%D0%B5%D0%B4%D0%B8%D0%B5_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8';
 
 function App() {
   useEffect(() => {
