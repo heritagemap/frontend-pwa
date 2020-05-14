c6f0700c91d29fc22d27414396d92ea843512dbf
Julia Lavrova <j.v.lavrova@gmail.com>
2020-05-14 21:58:52 +0300

    Load monuments on search city

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Map from 'components/Map';
 import './App.scss';
 
-const PAGES_RESOURCE = '/_api/heritage/?action=search&format=json&bbox=43.579856,56.062405,43.584620,56.064358'
-
 function App() {
-  useEffect(() => {
-    const loadPages = async () => {
-      try {
-        const response = await fetch(PAGES_RESOURCE);
-        const pages = await response.json();
-        console.log(pages);
-      } catch(err) {
-
-      }
-    }
-
-    loadPages();
-  });
-
   return (
     <main>
       <Map />
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,12 +1,13 @@
-import React, { Component, RefObject,  } from 'react';
+import React, { Component, RefObject } from 'react';
 // @ts-ignore
-import MapGL, { GeolocateControl, NavigationControl, InteractiveMap } from 'react-map-gl';
+import MapGL, { GeolocateControl, NavigationControl, InteractiveMap, Marker } from 'react-map-gl';
 // @ts-ignore
 import Geocoder from 'react-map-gl-geocoder';
 
 import styles from './MyMap.module.scss';
 
 const ACCESS_TOKEN ='pk.eyJ1IjoieXVsaWEtYXZkZWV2YSIsImEiOiJjazh0enUyOGEwNTR1M29va3I0YXMweXR5In0.6S0Dy1MTrzcgLlQEHtF2Aw';
+const PAGES_RESOURCE = '/_api/heritage/?action=search&format=json&bbox=';
 
 class MyMap extends Component {
   state = {
@@ -14,11 +15,33 @@ class MyMap extends Component {
       latitude: 56.80714671197797,
       longitude: 43.20772047711302,
       zoom: 14,
+      bearing: 0,
+      pitch: 0
     },
     searchValue: '',
+    monuments: [],
   };
 
   mapRef: RefObject<InteractiveMap> = React.createRef();
+
+  handleViewportChange = (viewport: any) => {
+    this.setState({viewport});
+  }
+
+  handleResult = async (result: { result: { bbox: [number] }}) => {
+    console.log(result)
+    try {
+      const response = await fetch(PAGES_RESOURCE + result.result.bbox.join());
+      const { monuments } = await response.json();
+
+       console.log(monuments);
+
+      this.setState({ monuments: monuments || [] });
+
+    } catch(err) {
+
+    }
+  }
 
   render() {
     return (
@@ -29,7 +52,7 @@ class MyMap extends Component {
         width="100vw"
         height="100vh"
         mapboxApiAccessToken={ACCESS_TOKEN}
-        onViewportChange={(viewport: any) => { this.setState({viewport}); console.log(viewport) }}
+        onViewportChange={this.handleViewportChange}
         mapStyle="mapbox://styles/mapbox/streets-v11"
       >
         <Geocoder
@@ -39,7 +62,8 @@ class MyMap extends Component {
           placeholder="Поиск"
           countries="ru"
           language="ru"
-          onViewportChange={(viewport: any) => { this.setState({viewport}); console.log(viewport) }}
+          onViewportChange={this.handleViewportChange}
+          onResult={this.handleResult}
         />
         <div className={styles.controls}>
           <GeolocateControl
@@ -48,6 +72,20 @@ class MyMap extends Component {
             trackUserLocation={true}
           />
           <NavigationControl showCompass={false} />
+
+          {this.state.monuments.map((item: { id: string, lon: number, lat: number, name: string }) => (
+            <Marker
+              key={item.id}
+              longitude={item.lon}
+              latitude={item.lat}
+            >
+              <svg height="24" width="24" viewBox="0 0 24 24" fill="#d00" stroke="none">
+                <path d="M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9C20.1,15.8,20.2,15.8,20.2,15.7z" />
+              </svg>
+
+              <span>{item.name}</span>
+            </Marker>
+          ))}
         </div>
       </MapGL>
     );
