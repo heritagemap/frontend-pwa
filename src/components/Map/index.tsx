dc78f02f37a3b2af88edb7e2096e863826081608
Julia Lavrova <j.v.lavrova@gmail.com>
2020-05-17 21:33:26 +0300

    Add info for points

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -6,6 +6,8 @@ import Geocoder from 'react-map-gl-geocoder';
 
 import styles from './MyMap.module.scss';
 
+import MarkerButton from 'components/MarkerButton';
+
 const ACCESS_TOKEN ='pk.eyJ1IjoieXVsaWEtYXZkZWV2YSIsImEiOiJjazh0enUyOGEwNTR1M29va3I0YXMweXR5In0.6S0Dy1MTrzcgLlQEHtF2Aw';
 const PAGES_RESOURCE = '/_api/heritage/?action=search&format=json&bbox=';
 
@@ -76,21 +78,13 @@ class MyMap extends Component {
           className={styles.navigationControl}
         />
 
-        {this.state.monuments.map((item: { id: string, lon: number, lat: number, name: string }) => (
+        {this.state.monuments.map((item: { id: string, lon: number, lat: number, name: string, address: string }) => (
           <Marker
             key={item.id}
             longitude={item.lon}
             latitude={item.lat}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384.2 384.2" width="20" height="20">
-              <path d="M320.1 144c0-70.72-57.28-128-128-128s-128 57.28-128 128 128 216 128 216 128-145.28 128-216zm-184-.08c0-30.96 25.04-56 56-56s56 25.04 56 56-25.04 56-56 56-56-25.04-56-56z" fill="#cce4ff"/>
-              <g fill="#007aff">
-                <path d="M264.1 143.92c0-39.696-32.296-72-72-72s-72 32.304-72 72 32.296 72 72 72 72-32.296 72-72zm-112 0c0-22.056 17.944-40 40-40s40 17.944 40 40-17.944 40-40 40-40-17.944-40-40z"/>
-                <path d="M192.1 384.2l12.008-13.624C217.612 355.24 336.1 218.608 336.1 144c0-79.4-64.6-144-144-144s-144 64.6-144 144c0 74.608 118.488 211.24 131.992 226.576L192.1 384.2zm0-352.2c61.76 0 112 50.24 112 112 0 47.648-72.32 143.968-112 191.4-39.68-47.432-112-143.752-112-191.4 0-61.76 50.24-112 112-112z"/>
-              </g>
-            </svg>
-
-            <span>{item.name}</span>
+            <MarkerButton item={item} />
           </Marker>
         ))}
       </MapGL>
