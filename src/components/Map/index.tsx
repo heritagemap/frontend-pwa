9bf37f3fe76cfb0e906847a3e9864259b5d6fbd0
Julia Lavrova <j.v.lavrova@gmail.com>
2020-05-17 20:53:53 +0300

    Fix bug with user location

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -12,9 +12,9 @@ const PAGES_RESOURCE = '/_api/heritage/?action=search&format=json&bbox=';
 class MyMap extends Component {
   state = {
     viewport: {
-      latitude: 56.80714671197797,
-      longitude: 43.20772047711302,
-      zoom: 14,
+      latitude: 56.6403,
+      longitude: 43.3865,
+      zoom: 10,
       bearing: 0,
       pitch: 0
     },
@@ -25,13 +25,12 @@ class MyMap extends Component {
   mapRef: RefObject<InteractiveMap> = React.createRef();
 
   handleViewportChange = (viewport: any) => {
-    this.setState({viewport});
+    this.setState({ viewport });
   }
 
   handleResult = async (result: { result: { bbox: [number] }}) => {
-    console.log(result)
     try {
-      const response = await fetch(PAGES_RESOURCE + result.result.bbox.join());
+      const response = await fetch(PAGES_RESOURCE + result.result.bbox.map(item => String(item).substr(0, 7)).join());
       const { monuments } = await response.json();
 
        console.log(monuments);
@@ -53,7 +52,7 @@ class MyMap extends Component {
         height="100vh"
         mapboxApiAccessToken={ACCESS_TOKEN}
         onViewportChange={this.handleViewportChange}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
       >
         <Geocoder
           mapRef={this.mapRef}
@@ -65,28 +64,35 @@ class MyMap extends Component {
           onViewportChange={this.handleViewportChange}
           onResult={this.handleResult}
         />
-        <div className={styles.controls}>
-          <GeolocateControl
-            style={{ marginBottom: '10px' }}
-            positionOptions={{ enableHighAccuracy: true }}
-            trackUserLocation={true}
-          />
-          <NavigationControl showCompass={false} />
 
-          {this.state.monuments.map((item: { id: string, lon: number, lat: number, name: string }) => (
-            <Marker
-              key={item.id}
-              longitude={item.lon}
-              latitude={item.lat}
-            >
-              <svg height="24" width="24" viewBox="0 0 24 24" fill="#d00" stroke="none">
-                <path d="M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9C20.1,15.8,20.2,15.8,20.2,15.7z" />
-              </svg>
+        <GeolocateControl
+          className={styles.geolocateControl}
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
+        />
 
-              <span>{item.name}</span>
-            </Marker>
-          ))}
-        </div>
+        <NavigationControl
+          showCompass={false}
+          className={styles.navigationControl}
+        />
+
+        {this.state.monuments.map((item: { id: string, lon: number, lat: number, name: string }) => (
+          <Marker
+            key={item.id}
+            longitude={item.lon}
+            latitude={item.lat}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384.2 384.2" width="20" height="20">
+              <path d="M320.1 144c0-70.72-57.28-128-128-128s-128 57.28-128 128 128 216 128 216 128-145.28 128-216zm-184-.08c0-30.96 25.04-56 56-56s56 25.04 56 56-25.04 56-56 56-56-25.04-56-56z" fill="#cce4ff"/>
+              <g fill="#007aff">
+                <path d="M264.1 143.92c0-39.696-32.296-72-72-72s-72 32.304-72 72 32.296 72 72 72 72-32.296 72-72zm-112 0c0-22.056 17.944-40 40-40s40 17.944 40 40-17.944 40-40 40-40-17.944-40-40z"/>
+                <path d="M192.1 384.2l12.008-13.624C217.612 355.24 336.1 218.608 336.1 144c0-79.4-64.6-144-144-144s-144 64.6-144 144c0 74.608 118.488 211.24 131.992 226.576L192.1 384.2zm0-352.2c61.76 0 112 50.24 112 112 0 47.648-72.32 143.968-112 191.4-39.68-47.432-112-143.752-112-191.4 0-61.76 50.24-112 112-112z"/>
+              </g>
+            </svg>
+
+            <span>{item.name}</span>
+          </Marker>
+        ))}
       </MapGL>
     );
   }
