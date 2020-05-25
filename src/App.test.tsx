8c86e894f9ca989ac4523c307ea1f6a9629b8469
Alexey Avdeev <aad.jerry@gmail.com>
2020-05-25 11:19:21 +0300

    test: add alert

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic'
 import App from './App';
 
 test('renders App', () => {
-  const { container } = render(<App />);
+  const { container } = render((
+    <AlertProvider template={AlertTemplate}>
+      <App />
+    </AlertProvider>
+  ));
   expect(container).toMatchSnapshot();
 });
