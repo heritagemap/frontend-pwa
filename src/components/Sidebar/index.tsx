d7392e81a2be6206bd55531da8bb6925064643e4
evgenykatyshev <me@ekatyshev.ru>
2020-11-11 18:16:43 +0300

    License info update

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -71,7 +71,7 @@ const Sidebar = () => {
         <FullInfo image={monument.image} />
       )}
       
-      <div className={styles.license}>Информация об объектах взята из <a href="https://ru.wikivoyage.org" target="_blank">Викигида</a><br /> Данные на сайте доступны по лицензии <a href="https://creativecommons.org/licenses/by-sa/3.0/deed.ru" target="_blank">CC-By-SA 3.0</a></div>
+      <div className={styles.license}>Информация об объектах взята из <a href="https://ru.wikivoyage.org" target="_blank">Викигида</a><br /> Эти данные доступны по лицензии <a href="https://creativecommons.org/licenses/by-sa/3.0/deed.ru" target="_blank">CC-By-SA 3.0</a></div>
     </section>
   )
 }
