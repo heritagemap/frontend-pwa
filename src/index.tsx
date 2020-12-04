2ae826a8252ca000ad7bb3deb5ca62e92be64dd6
Julia Lavrova <j.v.lavrova@gmail.com>
2020-12-04 14:06:13 +0300

    Check scss

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import AlertTemplate from 'react-alert-template-basic';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import './index.css';
+import './index.scss';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
